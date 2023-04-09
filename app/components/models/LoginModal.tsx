f12c0637fcdcd96a05a4a3e1b7b6d6b831147527
username <vasanthkumars2025@gmail.com>
2023-04-09 09:33:12 +0530

    added categories

--- app/components/models/LoginModal.tsx
+++ app/components/models/LoginModal.tsx
@@ -24,6 +24,7 @@ const LoginModal = () => {
   const router = useRouter();
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
+  
   const [isLoading, setIsLoading] = useState(false);
 
   const { 
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -4,13 +4,12 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-type Props = {};
-
-const Logo = (props: Props) => {
+const Logo = () => {
   const router = useRouter();
 
   return (
     <Image
+      onClick={() => router.push("/")}
       className="hidden md:block cursor-pointer"
       height="100"
       width="100"
